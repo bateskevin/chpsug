32bee28f14ebbb8f8dbbe9841555a90f913d2c7a
Kevin Bates <kevin96bates@gmail.com>
2019-10-16 15:48:38 +0200

    fied images on about

--- Bin/About.ps1
+++ Bin/About.ps1
@@ -44,7 +44,7 @@ $HTML = html {
             h1 -class 'display-4' -content "Powershell Usergroup Schweiz"
             hr -Class "my-4"
             
-            #img -src "https://3.bp.blogspot.com/-a7jPVdFk9Hw/W_XeTJX6JyI/AAAAAAAAC2c/HCtxP0wSSs0wEMKJOYq7pivEJaSVin92gCLcBGAs/s1600/powershell.png" -alt "Powershell Logo" -height "250" -width "250" -Class "img-responsive"
+            img -src "https://3.bp.blogspot.com/-a7jPVdFk9Hw/W_XeTJX6JyI/AAAAAAAAC2c/HCtxP0wSSs0wEMKJOYq7pivEJaSVin92gCLcBGAs/s1600/powershell.png" -alt "Powershell Logo" -height "250" -width "250" -Class "img-responsive"
             
             p -Class "lead" -Content "Online Meetups mit deinen Powershell Peers" -Style "color:#d5caf7;"
 
@@ -65,7 +65,7 @@ $HTML = html {
                     "About Powershell User Group Switzerland"
                 }
 
-                img -src "https://avatars0.githubusercontent.com/u/31886373?s=460&v=4" -alt "Kevin Bates" -height "300" -width "300" -Class "rounded-circle"
+                #img -src "https://avatars0.githubusercontent.com/u/31886373?s=460&v=4" -alt "Kevin Bates" -height "300" -width "300" -Class "rounded-circle"
 
 
                 p "Die Usergruppe wurde im Oktober 2019 gegründet und hält monatlich Online meetups über verschiedene Technologien im Zusammenhang mit Powershell."
